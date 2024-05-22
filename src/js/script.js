367f39041dbf78ff6e0fbb348069f6af3fd5c170
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-22 13:59:52 -0300

    javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
+const form = document.querySelector(".formulario-fale-conosco");
+const mascara = document.querySelector(".mascara-form");
 
+function mostrarForm() {
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  mascara.style.visibility = "visible";
+}
+
+function fecharForm() {
+  form.style.left = "-295px";
+  form.style.transform = "translateX(0)";
+  mascara.style.visibility = "hidden";
+}
